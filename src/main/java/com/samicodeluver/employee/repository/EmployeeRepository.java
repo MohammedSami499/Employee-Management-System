44ca2fb997be48a3ad837fbaeb295213f607db42
Mohammed Sami <mshelal555@gmail.com>
2022-10-01 22:57:55 +0200

    Initial commit of Java Spring boot Employee Management System

---
+++ src/main/java/com/samicodeluver/employee/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.samicodeluver.employee.repository;
+
+import com.samicodeluver.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+}
